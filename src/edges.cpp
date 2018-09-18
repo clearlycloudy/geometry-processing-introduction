312c5a263e17f6443abc045a4d86e9d76f799afd
Bill Liu <yuan6liu@gmail.com>
2018-09-18 16:13:32 -0400

    added changes for homework00.

--- src/edges.cpp
+++ src/edges.cpp
@@ -1,8 +1,40 @@
 #include "edges.h"
 
+#include <set>
+#include <algorithm>
+#include <utility>
+#include <cassert>
+
 Eigen::MatrixXi edges(const Eigen::MatrixXi &F)
 {
   Eigen::MatrixXi E;
   // ADD YOUR CODE HERE
+
+  std::set<std::pair<int,int>> s;
+  
+  int num_rows = F.rows();
+
+  //assumes faces are triangles
+  for( int i = 0 ; i < num_rows; ++i ){
+
+      //map pairs of sorted vert ids -> undirected edge
+      int arr[3] = { F(i,0), F(i,1), F(i,2) };
+
+      std::sort(arr,arr+3);
+
+      s.insert( {arr[0],arr[1]} );
+      s.insert( {arr[1],arr[2]} );
+      s.insert( {arr[0],arr[2]} );
+  }
+
+  E.resize( s.size(), 2 );
+
+  int row = 0;
+  for( auto & i : s ){
+      E(row,0) = i.first;
+      E(row,1) = i.second;
+      ++row;
+  }
+    
   return E;
 }
--- src/euler_characteristic.cpp
+++ src/euler_characteristic.cpp
@@ -1,8 +1,30 @@
 #include "euler_characteristic.h"
+#include "edges.h"
+
+#include <unordered_set>
 
 int euler_characteristic(const Eigen::MatrixXi &F)
 {
   int Chi = 0;
   // ADD YOUR CODE HERE
+
+  Eigen::MatrixXi e = edges(F);
+
+  int num_faces = F.rows();
+
+  int num_edges = e.rows();
+
+  std::unordered_set<int> s;
+
+  for( int i = 0; i < num_faces; ++i ){
+      s.insert( F(i,0) );
+      s.insert( F(i,1) );
+      s.insert( F(i,2) );
+  }
+
+  int num_verts = s.size();
+
+  Chi = num_verts - num_edges + num_faces;
+  
   return Chi;
 }
